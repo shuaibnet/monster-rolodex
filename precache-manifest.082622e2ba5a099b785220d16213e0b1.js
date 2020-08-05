1e68675400d2f14cb03a801797a3d5f78d816a51
shuaibnet <abir728@gmail.com>
2020-08-05 23:31:48 +0600

    Updates

---
+++ precache-manifest.082622e2ba5a099b785220d16213e0b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f63218a704327de36dc22f60b191c1f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1003f14824e69a757f1c",
+    "url": "/monster-rolodex/static/css/main.4b5ef215.chunk.css"
+  },
+  {
+    "revision": "381703caab323eca7b81",
+    "url": "/monster-rolodex/static/js/2.2d00c345.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.2d00c345.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1003f14824e69a757f1c",
+    "url": "/monster-rolodex/static/js/main.124a5194.chunk.js"
+  },
+  {
+    "revision": "ecc254ee3f36ced30b5d",
+    "url": "/monster-rolodex/static/js/runtime-main.ea49d851.js"
+  }
+]);
