13a277ce3ff66871100cd84d4191f88e23b9044b
shuaibnet <abir728@gmail.com>
2020-08-07 20:37:04 +0600

    Added Template literals

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.style.css';
 export const Card = props => (
     <div className='card-container'>
         <img alt='moster'
-            src={'https://robohash.org/${props.monster.id}?set=set2&size=180x180'} />
+            src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
         <h3>{props.monster.name}</h3>
         <p>{props.monster.email}</p>
     </div >
